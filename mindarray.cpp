af947324986b9e52b6ba5b6b23bd1761ce23eaab
Mohammad ali mazdarani <74422713+mohammadali-mazdarani@users.noreply.github.com>
2022-12-18 04:54:16 -0800

    initial commit

---
+++ mindarray.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system(
+) or input loop */
+/*branch name
+*
+*
+*
+*/
+//prototype
+//--------------------------
+//micro
+//---------------------
+//global variable
+//-------------------
+int main(int argc, char** argv)
+{
+
+
+getch();
+}
+//function variable
+//--------------------------
+//functions
